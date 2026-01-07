7e0929c919e09ebd5b9709bf176b499d904a3563
HaiderChishty <haider.chishty97@gmail.com>
2026-01-07 15:36:14 -0500

    counting num passengers in elevator

--- Algorithm_Challenge/Elevator/elevator_problem.c
+++ Algorithm_Challenge/Elevator/elevator_problem.c
@@ -46,7 +46,19 @@ static void delay(int16_t ms);
 //Note: The output should be a number between 0 and (BUILDING_HEIGHT-1), inclusive
 static int8_t setNextElevatorStop(struct building_s building)
 {
-	return 0;
+	// calculate num passengers
+	int passengerCount = 0;
+	for (int i = 0; i < ELEVATOR_MAX_CAPACITY; i++)
+	{
+		if (building.elevator.passengers[i] != -1)
+		{
+			passengerCount++;
+		}
+	}
+	printf("Passenger Count: %d\n", passengerCount);
+	delay(1000);
+
+	return 1;
 }
 
 
